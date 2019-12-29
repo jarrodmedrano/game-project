e8ef37350bd4041bae779640c3497bc91e7446cd
Jarrod Medrano <jmedran@gmail.com>
2019-12-29 11:08:48 -0700

    draw trees

--- sketch.js
+++ sketch.js
@@ -35,7 +35,50 @@ let isLeft = false,
 */
 
 const groundLevel = 432;
+const floorPos_y = groundLevel;
+const mountains = [
+	{
+		pos_x: 300,
+		height: 400
+	},
+	{
+		pos_x: 150,
+		height: 300
+	},
+	{
+		pos_x: 500,
+		height: 200
+	},
+	{
+		pos_x: 1000,
+		height: 200
+	},
+	{
+		pos_x: 1500,
+		height: 400
+	}
+];
 
+const clouds = [
+	{
+		pos_x: 100,
+		pos_y: 100
+	},
+	{
+		pos_x: 600,
+		pos_y: 150
+	},
+	{
+		pos_x: 1000,
+		pos_y: 200
+	}
+]
+
+const trees_x = [
+	200,
+	100,
+	1000
+]
 /**
  * Character Stuff
  */
@@ -49,7 +92,7 @@ let gameChar_y = gameCharYStart;
 
 function drawCharacter() {
 	noStroke();
-	fill(139, 69, 19);
+	fill(0, 0, 128);
 	ellipse(gameChar_x, gameChar_y, 55, gameCharHeight);
 	fill(255, 255, 255);
 	ellipse(gameChar_x, gameChar_y, 10, 10);
@@ -59,7 +102,7 @@ function drawCharacter() {
 
 function drawCharacterLeft() {
 	noStroke();
-	fill(139, 69, 19);
+	fill(0, 0, 128);
 	ellipse(gameChar_x, gameChar_y, 55, gameCharHeight);
 	fill(255, 255, 255);
 	ellipse(gameChar_x - 20, gameChar_y, 10, 10);
@@ -69,7 +112,7 @@ function drawCharacterLeft() {
 
 function drawCharacterRight() {
 	noStroke();
-	fill(139, 69, 19);
+	fill(0, 0, 128);
 	ellipse(gameChar_x, gameChar_y, 55, gameCharHeight);
 	fill(255, 255, 255);
 	ellipse(gameChar_x + 20, gameChar_y, 10, 10);
@@ -77,88 +120,74 @@ function drawCharacterRight() {
 	ellipse(gameChar_x + 10, gameChar_y, 10, 10);
 }
 
+function drawGround() {
+	noStroke();
+	fill(0, 155, 0);
+	rect(0, groundLevel, 1024, 144); //draw some green ground
+}
+
+function drawMountain(pos_x, height) {
+	noStroke();
+	fill(0, 118, 255);
+	triangle(
+		pos_x - height / 2, floorPos_y,
+		pos_x, floorPos_y - height,
+		pos_x + 45 + height / 2, floorPos_y
+	)
+}
+
+function drawTree(pos_x) {
+	noStroke();
+	fill(139, 69, 19);
+	triangle(pos_x, 432, pos_x + 150, 432, pos_x + 70, 225);
+	fill(0, 155, 0);
+	triangle(pos_x, 250, pos_x + 150, 250, pos_x + 70, 200);
+	triangle(pos_x, 300, pos_x + 150, 300, pos_x + 70, 225);
+}
+
+function drawCanyon() {
+	noStroke();
+	fill(139, 69, 19);
+	triangle(70, 432, 200, 432, 130, 600);
+}
+
+
+function drawSky() {
+	background(100, 155, 255);
+}
+
+
+
+function drawCloud(pos_x, pos_y) {
+	noStroke();
+	fill(255);
+	ellipse(pos_x, pos_y, 100);
+	ellipse(pos_x + 50, pos_y - 40, 100, 100);
+	ellipse(pos_x + 50, pos_y, 100, 100);
+	ellipse(pos_x + 100, pos_y, 100, 100);
+}
+
+
 function setup() {
-
-	function drawGround() {
-		noStroke();
-		fill(0, 155, 0);
-		rect(0, groundLevel, 1024, 144); //draw some green ground
-	}
-
-	function drawCloud() {
-		noStroke();
-		fill(255);
-		ellipse(100, 100, 100);
-		ellipse(150, 60, 100, 100);
-		ellipse(150, 100, 100, 100);
-		ellipse(200, 100, 100, 100);
-		text("cloud", 200, 100);
-	}
-
-	function drawMountain() {
-		noStroke();
-		fill(70, 130, 180);
-		triangle(380, 432, 600, 232, 930, 432);
-		fill(70, 130, 180);
-		triangle(240, 432, 450, 332, 930, 432);
-		fill(70, 130, 180);
-		triangle(640, 432, 850, 332, 1030, 432);
-	}
-
-	function drawTree() {
-		noStroke();
-		fill(139, 69, 19);
-		triangle(750, 432, 900, 432, 820, 225);
-		fill(0, 155, 0);
-		triangle(750, 250, 900, 250, 820, 200);
-		triangle(750, 300, 900, 300, 820, 225);
-	}
-
-	function drawCanyon() {
-		noStroke();
-		fill(139, 69, 19);
-		triangle(70, 432, 200, 432, 130, 600);
-	}
-
-
-	function drawSky() {
-		background(100, 155, 255);
-	}
-
 	createCanvas(1024, 576);
+}
 
+function draw() {
 	drawSky();
 
 	drawGround();
 
-
-}
-
-function keyPressed() {
-	if (keyCode == 37) {
-		console.log('left arrow', isLeft);
-		isLeft = true;
+	for (let i = 0; i < clouds.length; i++) {
+		drawCloud(clouds[i].pos_x, clouds[i].pos_y);
 	}
 
-	if (keyCode == 39) {
-		console.log('right arrow', isRight);
-		isRight = true;
-	}
-}
-
-function keyReleased() {
-	if (keyCode == 37) {
-		console.log('left arrow', isLeft);
-		isLeft = false;
+	for (let i = 0; i < mountains.length; i++) {
+		drawMountain(mountains[i].pos_x, mountains[i].height);
 	}
 
-	if (keyCode == 39) {
-		console.log('right arrow', isRight);
-		isRight = false;
+	for (let i = 0; i < trees_x.length; i++) {
+		drawTree(trees_x[i]);
 	}
-}
-
-function draw() {
 
 
 	if (isLeft && isFalling) {
@@ -176,17 +205,69 @@ function draw() {
 		drawCharacter();
 	}
 
-	if (isLeft == true) {
-		gameChar_x -= 10;
-	}
+
 
 	if (isRight == true) {
-		gameChar_x += 10;
+		gameChar_x += 2;
+		if (gameChar_x >= width) {
+			gameChar_x = width;
+		}
+	}
+	else if (isLeft == true) {
+		gameChar_x -= 2;
+		if (gameChar_x <= 0) {
+			gameChar_x = 0;
+		}
+	}
+	else if (isPlummeting == true) {
+		gameChar_y += 5;
 	}
 
-	push();
-	translate(440, 350);
-	rotate(frameCount / -100.0);
-	fill(255, 255, 0);
-	pop();
+	// if (gameChar_y < floorPos_y) {
+	// 	gameChar_y += 2;
+	// 	isFalling = true;
+	// }
+	// else {
+	// 	isFalling = false;
+	// }
+	//console.log(gameChar_x, canyon.x_pos);
+	// if (gameChar_x >= canyon.x_pos - 20 && gameChar_x <= canyon.x_pos + 10 && isFalling == false) {
+	// 	isPlummeting = true;
+	// 	isRight = false;
+	// 	isLeft = false;
+	// }
+
+
+
+	// push();
+	// translate(440, 350);
+	// rotate(frameCount / -100.0);
+	// fill(255, 255, 0);
+	// pop();
+}
+
+function keyPressed() {
+	if (keyCode == 37) {
+		console.log('left arrow', isLeft);
+		isLeft = true;
+		isRight = false;
+	}
+
+	if (keyCode == 39) {
+		console.log('right arrow', isRight);
+		isRight = true;
+		isLeft = false;
+	}
+}
+
+function keyReleased() {
+	if (keyCode == 37) {
+		console.log('left arrow', isLeft);
+		isLeft = false;
+	}
+
+	if (keyCode == 39) {
+		console.log('right arrow', isRight);
+		isRight = false;
+	}
 }
